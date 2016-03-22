eefd41a51f871681490322263406f8e175c92b74
ryancharris <harrisryan1@gmail.com>
2016-03-22 14:32:09 -0400

    Finished 'Getting Started with Express'

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+// GET home route
+app.get('/', function(req, res){
+  res.send("<h1>I love Treehouse!</h1>");
+});
+
+app.listen(3000, function(){
+  console.log("The front-end server is running on port 3000!");
+
+});
