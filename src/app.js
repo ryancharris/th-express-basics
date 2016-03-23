7faa39d28f0dff836dbdeffd145ad13e955d82e3
ryancharris <harrisryan1@gmail.com>
2016-03-23 15:25:46 -0400

    Finished Adding Routes to the App

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    posts = require('./mock/posts.json');
 
 var app = express();
 
 app.get('/', function(req, res){
 	res.send("<h1>I Love Treehouse!</h1>");
 });
+
+app.get('/blog', function(req, res) {
+  res.send(posts);
+})
 
 app.listen(3000, function(){
 	console.log("The frontend server is running on port 3000!")
